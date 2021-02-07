a6f322d92da0ed46249f4f1d60d4d9edbc033705
IgorBashkov <bashkov.torvm36@gmail.com>
2021-02-07 19:56:21 +0300

    Added timer. Some classes moved to new module

---
+++ elements.py
@@ -0,0 +1,130 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+
+
+class AddMenu:
+    def __init__(self, root):
+
+        root.resizable(False, False)
+        root.option_add('*tearOff', False)
+        menu_bar = tk.Menu(root)
+        root['menu'] = menu_bar
+        menu_game = tk.Menu(menu_bar)
+        menu_results = tk.Menu(menu_bar)
+        menu_about = tk.Menu(menu_bar)
+        menu_bar.add_cascade(menu=menu_game, label='Game')
+        menu_bar.add_cascade(menu=menu_results, label='Results')
+        menu_bar.add_cascade(menu=menu_about, label='About')
+        hardness = tk.StringVar()
+        commands = ('Easy', 'Medium', 'Hard', 'Custom')
+        for dif in commands:
+            menu_game.add_radiobutton(
+                label=dif,
+                variable=hardness,
+                value=dif,
+                command=lambda: root.event_generate(f'<<{hardness.get()}>>'),
+            )
+            menu_results.add_command(label=dif, command=lambda: True)
+
+
+class Cell(ttk.Frame):
+    text_color = {'1': 'blue', '2': 'green', '3': 'red',
+                  '4': 'brown', '5': 'pink', '6': 'crimson',
+                  '7': 'orange', '8': 'coral', '9': 'black',
+                  }
+
+    def __init__(self, root, lbl_text='', pos_x=0, pos_y=0):
+        # self.style = ttk.Style()
+        # self.style.theme_use('default')
+        self.flag = tk.PhotoImage(file='static/flag.gif')
+        self.mine = tk.PhotoImage(file='static/mine2.gif')
+        text_font = font.Font(weight='bold', size=12)
+        super().__init__(master=root, width=20, height=20, borderwidth=0)
+        self.rowconfigure(0, weight=0)
+        self.columnconfigure(0, weight=0)
+        # self.configure(background='#6F6F6F')
+        self.root = root
+        self.text = lbl_text
+        self.opened = False
+        self.bomb = True if lbl_text == '9' else False
+        self.mark_bomb = False
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.area = self.root.field.mapper_dict[(self.pos_y, self.pos_x)]
+
+        self.lbl_cell = ttk.Label(self,
+                                  text=lbl_text,
+                                  foreground=Cell.text_color.get(lbl_text, 'black'),
+                                  width=3,
+                                  anchor='center',
+                                  font=text_font,
+                                  compound='image' if self.bomb else 'text',
+                                  image=self.mine
+                                  # relief='groove',
+                                  )
+
+        self.lbl_cell.grid(sticky='swen', column=0, row=0,
+                           ipady=0 if self.bomb else 6,
+                           ipadx=0 if self.bomb else 2)
+
+        self.btn_cell = ttk.Button(self,
+                                   width=3,
+                                   command=self.open,
+                                   image=self.flag,
+                                   compound='text',
+                                   )
+        self.btn_cell.grid(sticky='swen', column=0, row=0)
+        self.lbl_cell.bind('<ButtonRelease-1><ButtonRelease-3>', self.combo)
+        self.lbl_cell.bind('<ButtonPress-1><ButtonPress-3>', self.aim_area)
+        self.btn_cell.bind('<ButtonPress-3>', self.mark)
+
+    def open(self, *args):
+        if not self.mark_bomb and not self.opened:
+            self.event_generate('<<game_started>>')
+            self.opened = True
+            self.root.closed -= 1
+            self.btn_cell.destroy()
+            y, x = int(self.grid_info()['column']), int(self.grid_info()['row'])
+            if not self.text:
+                for i, j in self.area:
+                    self.root.cells[i][j].open()
+            if self.text == '9':
+                self.lbl_cell.configure(background='red')
+                self.root.event_generate('<<game_over>>')
+            if not self.root.closed:
+                self.root.event_generate('<<you_win!>>')
+                print('Win')
+
+    def mark(self, *args):
+        self.mark_bomb = not self.mark_bomb
+        if self.mark_bomb:
+            self.root.event_generate('<<mark_added>>')
+        else:
+            self.root.event_generate('<<mark_deleted>>')
+        self.btn_cell.configure(compound='image' if self.mark_bomb else 'text')
+
+    def combo(self, *args):
+        self.unpressed()
+        marks = sum(self.root.cells[x][y].mark_bomb for x, y in self.area)
+        if self.lbl_cell['text'] and marks == int(self.lbl_cell['text']):
+            for x, y in self.area:
+                if not self.root.cells[x][y].opened:
+                    self.root.cells[x][y].open()
+
+    def aim_area(self, args):
+        print(args.type)
+        if 'ButtonPress' in args.type:
+            self.root.event_generate('<<show_aim>>')
+
+        for x, y in self.area:
+            if not self.root.cells[x][y].opened and not self.root.cells[x][y].mark_bomb:
+                self.root.cells[x][y].btn_cell.state(['pressed'])
+
+    def unpressed(self, *args):
+
+        if not args:
+            self.root.event_generate('<<hide_aim>>')
+        for x, y in self.area:
+            if not self.root.cells[x][y].opened and not self.root.cells[x][y].mark_bomb:
+                self.root.cells[x][y].btn_cell.state(['!pressed'])
--- layout_classes.py
+++ layout_classes.py
@@ -1,136 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 from logic_classes import Field
-from tkinter import font
-
-
-class AddMenu:
-
-    def __init__(self, root):
-
-        root.resizable(False, False)
-        root.option_add('*tearOff', False)
-        menu_bar = tk.Menu(root)
-        root['menu'] = menu_bar
-        menu_game = tk.Menu(menu_bar)
-        menu_results = tk.Menu(menu_bar)
-        menu_about = tk.Menu(menu_bar)
-        menu_bar.add_cascade(menu=menu_game, label='Game')
-        menu_bar.add_cascade(menu=menu_results, label='Results')
-        menu_bar.add_cascade(menu=menu_about, label='About')
-        hardness = tk.StringVar()
-        commands = ('Easy', 'Medium', 'Hard', 'Custom')
-        for dif in commands:
-            menu_game.add_radiobutton(
-                label=dif,
-                variable=hardness,
-                value=dif,
-                command=lambda: root.event_generate(f'<<{hardness.get()}>>'),
-            )
-            menu_results.add_command(label=dif, command=lambda: True)
-
-
-class Cell(ttk.Frame):
-    text_color = {'1': 'blue', '2': 'green', '3': 'red',
-                  '4': 'brown', '5': 'pink', '6': 'crimson',
-                  '7': 'orange', '8': 'coral', '9': 'black',
-                  }
-
-    def __init__(self, root, lbl_text='', pos_x=0, pos_y=0):
-        # self.style = ttk.Style()
-        # self.style.theme_use('default')
-        self.flag = tk.PhotoImage(file='static/flag.gif')
-        self.mine = tk.PhotoImage(file='static/mine2.gif')
-        text_font = font.Font(weight='bold', size=12)
-        super().__init__(master=root, width=20, height=20, borderwidth=0)
-        self.rowconfigure(0, weight=0)
-        self.columnconfigure(0, weight=0)
-        # self.configure(background='#6F6F6F')
-        self.root = root
-        self.text = lbl_text
-        self.opened = False
-        self.bomb = True if lbl_text == '9' else False
-        self.mark_bomb = False
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.area = self.root.field.mapper_dict[(self.pos_y, self.pos_x)]
-        self.pressed = False
-
-        self.lbl_cell = ttk.Label(self,
-                                  text=lbl_text,
-                                  foreground=Cell.text_color.get(lbl_text, 'black'),
-                                  width=3,
-                                  anchor='center',
-                                  font=text_font,
-                                  compound='image' if self.bomb else 'text',
-                                  image=self.mine
-                                  # relief='groove',
-                                  )
-
-        self.lbl_cell.grid(sticky='swen', column=0, row=0,
-                           ipady=0 if self.bomb else 6,
-                           ipadx=0 if self.bomb else 2)
-
-        self.btn_cell = ttk.Button(self,
-                                   width=3,
-                                   command=self.open,
-                                   image=self.flag,
-                                   compound='text',
-                                   )
-        self.btn_cell.grid(sticky='swen', column=0, row=0)
-
-        self.lbl_cell.bind('<ButtonRelease-1><ButtonRelease-3>', self.combo)
-        # self.bind('<ButtonRelease-1><ButtonRelease-3>', self.unpressed)
-        self.lbl_cell.bind('<ButtonPress-1><ButtonPress-3>', self.aim_area)
-        self.lbl_cell.bind('<Leave>', self.unpressed)
-        self.lbl_cell.bind('<Enter><ButtonPress-1><ButtonPress-3>', self.aim_area)
-        self.btn_cell.bind('<Leave>', self.unpressed)
-        self.btn_cell.bind('<Enter><ButtonPress-1><ButtonPress-3>', self.aim_area)
-        self.btn_cell.bind('<ButtonPress-3>', self.mark)
-
-    def open(self, *args):
-        if not self.mark_bomb and not self.opened:
-            self.event_generate('<<game_started>>')
-            self.opened = True
-            self.root.closed -= 1
-            self.btn_cell.destroy()
-            y, x = int(self.grid_info()['column']), int(self.grid_info()['row'])
-            if not self.text:
-                for i, j in self.area:
-                    self.root.cells[i][j].open()
-            if self.text == '9':
-                self.lbl_cell.configure(background='red')
-                self.root.event_generate('<<game_over>>')
-            if not self.root.closed:
-                self.root.event_generate('<<you_win!>>')
-                print('Win')
-
-    def mark(self, *args):
-        self.mark_bomb = not self.mark_bomb
-        if self.mark_bomb:
-            self.root.event_generate('<<mark_added>>')
-        else:
-            self.root.event_generate('<<mark_deleted>>')
-        self.btn_cell.configure(compound='image' if self.mark_bomb else 'text')
-
-    def combo(self, *args):
-        self.unpressed()
-        self.pressed = False
-        marks = sum(self.root.cells[x][y].mark_bomb for x, y in self.area)
-        if marks == int(self.lbl_cell['text']):
-            for x, y in self.area:
-                if not self.root.cells[x][y].opened:
-                    self.root.cells[x][y].open()
-
-    def aim_area(self, *args):
-        for x, y in self.area:
-            if not self.root.cells[x][y].opened and not self.root.cells[x][y].mark_bomb:
-                self.root.cells[x][y].btn_cell.state(['pressed'])
-
-    def unpressed(self, *args):
-        for x, y in self.area:
-            if not self.root.cells[x][y].opened and not self.root.cells[x][y].mark_bomb:
-                self.root.cells[x][y].btn_cell.state(['!pressed'])
+from elements import AddMenu, Cell
+import threading
+import time
 
 
 class GameField(ttk.Frame):
@@ -141,6 +14,7 @@ class GameField(ttk.Frame):
         self.field = Field(*field_size, mines)
         self.closed = field_size[0] * field_size[1] - mines
         self.cells = []
+        self.pressed = False
         for i, lst in enumerate(self.field.field):
             self.cells.append([])
             for j, num in enumerate(lst):
@@ -159,18 +33,13 @@ class GameField(ttk.Frame):
             cell = self.grid_slaves(row=y, column=x)[0]
             cell.opened = True
             cell.btn_cell.destroy()
-            # cell.lbl_cell.configure(background='red')
         self.unbind_open()
 
     def unbind_open(self, *args):
-        print(args)
         for cell in self.grid_slaves():
             if not cell.opened:
-
                 cell.btn_cell.unbind('<ButtonPress-1>')
                 cell.btn_cell.unbind('<ButtonPress-3>')
-
-                # cell.event_generate('<<mark_added>>') if not args else None
                 cell.btn_cell.configure(compound='text' if not args else 'image')
                 cell.btn_cell.state(['disabled'])
 
@@ -187,6 +56,10 @@ class MainWindow:
         self.buttons = []
         self.bombs_num = 10
         self.remaining_mines = tk.IntVar()
+        self.time = tk.IntVar()
+        self.start_time = None
+        self.end_time = None
+        self.clock = None
         self.remaining_mines.set(self.bombs_num)
         self.field_size = (10, 10)
 
@@ -195,7 +68,8 @@ class MainWindow:
         self.frm_game.rowconfigure(0, weight=0, minsize=70)
         self.frm_game.columnconfigure((0, 1, 2), weight=1, minsize=93)
 
-        self.lbl_clock = ttk.Label(self.frm_game, text='Time\nWill be\nhere')
+        self.lbl_clock = ttk.Label(self.frm_game, textvariable=self.time)
+
         self.lbl_clock.grid(column=0, row=0, padx=5, pady=5)
 
         self.btn_start = ttk.Button(self.frm_game, text='Wait\nfor\nmove', command=self.restart)
@@ -222,6 +96,7 @@ class MainWindow:
         self.root.bind('<<Medium>>', lambda e: self.options((15, 20, 40)))
         self.root.bind('<<Hard>>', lambda e: self.options((30, 20, 100)))
         self.root.bind('<<Custom>>', lambda e: self.options((10, 10, 10)))
+
         root.rowconfigure(2, weight=1)
 
     def options(self, opt=(10, 10, 10), *args):
@@ -242,29 +117,35 @@ class MainWindow:
                                 field_size=self.field_size)
         self.frm_field.grid(column=0, row=1, padx=5, pady=5, sticky='swen')
         self.remaining_mines.set(self.bombs_num)
-        self.lbl_clock.configure(text='Time\nWill be\nhere')
         self.root.bind('<<game_started>>', self.start)
+        self.time.set(0)
         self.btn_start.configure(text='Have\nFun!')
 
     def end(self, *args):
+        self.clock.cancel()
         self.btn_start.configure(text='Game\nOver!!!\nAgain?')
-        self.lbl_clock.configure(text='End')
+        self.lbl_clock.unbind('<<Rise_time>>')
         print('Over')
 
     def win(self, *args):
+        self.clock.cancel()
         self.btn_start.configure(text='You\nWin!!!\nAgain?')
-        self.lbl_clock.configure(text='End')
-        print('Over')
+        self.lbl_clock.unbind('<<Rise_time>>')
+        self.end_time = time.time() - self.start_time
+        print(f'Over by {self.end_time}')
 
     def start(self, *args):
-        self.lbl_clock.configure(text='Tic-Tac')
+        self.tik_tak()
+        self.start_time = time.time()
+        self.lbl_clock.bind('<<Rise_time>>',
+                            lambda e: self.time.set(self.time.get() + 1))
         self.root.unbind('<<game_started>>')
         print('GoGoGo!')
 
-
-
-
-
-
+    def tik_tak(self):
+        self.clock = threading.Timer(1, self.tik_tak)
+        self.clock.daemon = True
+        self.lbl_clock.event_generate('<<Rise_time>>')
+        self.clock.start()
 
 
